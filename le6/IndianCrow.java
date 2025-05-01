857f3d80878068465b3a140c43c5b24a777a33a7
221B486 <221B486@juet.co.in>
2025-05-01 14:51:29 +0530

    adapter functiom

---
+++ le6/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("crawing");
+    }
+    public void fly(){
+        System.out.println("fly over the mountains");
+    }
+    public void eat(){
+        System.out.println("eat pearls");
+    }
+}
